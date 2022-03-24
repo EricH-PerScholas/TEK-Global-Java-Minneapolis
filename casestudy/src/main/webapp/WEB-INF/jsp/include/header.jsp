273cd6a3547d3f20c2d056d15154067b79c88194
Eric Heilig <eheilig@perscholas.org>
2022-03-24 13:03:00 -0600

    our first spring boot project

---
+++ casestudy/src/main/webapp/WEB-INF/jsp/include/header.jsp
@@ -0,0 +1,10 @@
+<html>
+<head>
+</head>
+
+<body>
+
+<a href="/">Index</a> | <a href="/sba">SBA</a>
+<!-- put your nav bar here -->
+
+
---
+++ hibernate/src/main/resources/db/migration/V0.0.01__Schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `actors` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(45) NOT NULL,
+  `last_name` varchar(45) NOT NULL,
+  `age` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+ALTER TABLE `perscholas`.`actors`
+ADD COLUMN `sex` VARCHAR(45) NULL AFTER `age`;
