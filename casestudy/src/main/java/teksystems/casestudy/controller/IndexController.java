538501ef0efb6d056960101c25c624140339db63
Eric Heilig <eheilig@perscholas.org>
2022-04-07 10:53:55 -0600

    added logging configuration

--- casestudy/src/main/java/teksystems/casestudy/controller/IndexController.java
+++ casestudy/src/main/java/teksystems/casestudy/controller/IndexController.java
@@ -1,6 +1,8 @@
 package teksystems.casestudy.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,12 +10,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import teksystems.casestudy.database.dao.UserDAO;
 import teksystems.casestudy.database.entity.User;
+import teksystems.casestudy.validation.EmailUniqueImpl;
 
 import java.util.List;
 
 @Slf4j
 @Controller
 public class IndexController {
+
 
     @Autowired
     private UserDAO userDao;
@@ -22,10 +26,14 @@ public class IndexController {
     public ModelAndView index() throws Exception {
         ModelAndView response = new ModelAndView();
 
+      // log.debug("lombok logging at debug level");
+       log.info("lombok logging at info level");
+       log.warn("lombok logging at info level");
+
         List<User> users = userDao.findByFirstNameIgnoreCaseContaining("A");
 
         for( User user : users ) {
-            log.info(user.toString());
+            log.debug(user.toString());
         }
 
         // figure out how to add this to the model
