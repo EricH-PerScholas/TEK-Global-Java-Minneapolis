e155ee1fa6cfd0de15bcbcaa536e5090f216d98f
Eric Heilig <eheilig@perscholas.org>
2022-04-15 13:39:50 -0600

    step 3 added binding result

--- casestudy/src/main/java/teksystems/casestudy/controller/ConversationController.java
+++ casestudy/src/main/java/teksystems/casestudy/controller/ConversationController.java
@@ -2,12 +2,16 @@ package teksystems.casestudy.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import teksystems.casestudy.database.entity.User;
 import teksystems.casestudy.formbean.ConversationFormBean;
+
+import javax.validation.Valid;
 
 @Slf4j
 @Controller
@@ -22,9 +26,15 @@ public class ConversationController {
 
 
     @RequestMapping(value = "/conversation/conversationSubmit", method = RequestMethod.GET)
-    public ModelAndView submit(ConversationFormBean form) throws Exception {
+    public ModelAndView submit(@Valid ConversationFormBean form, BindingResult bindingResult) throws Exception {
         ModelAndView response= new ModelAndView();
         response.setViewName("conversation");
+
+        if (bindingResult.hasErrors())  {
+            for ( FieldError error : bindingResult.getFieldErrors()) {
+                log.debug(error.toString());
+            }
+        }
 
         log.debug(form.toString());
 
